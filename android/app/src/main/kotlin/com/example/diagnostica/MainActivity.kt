a92d6f61de6d74de1cf1433fae09c17ceccb49ab
SantiagoJapon <sajaponpa@uide.edu.ec>
2023-12-21 18:08:02 -0500

    prueba diagnostica

---
+++ android/app/src/main/kotlin/com/example/diagnostica/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.diagnostica
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/Persona.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+//CREAMOS LOS ATRIBUTOS DE LA CLASE
+class Persona {
+  String nombre;
+  String apellido;
+  int edad;
+
+  //SE CREA EL CONSTRUCTOR
+
+  Persona({required this.nombre, required this.apellido, required this.edad});
+
+  factory Persona.fromEdad({required int edad}) {
+    return Persona(nombre: 'Santiago', apellido: 'Japon', edad: edad);
+  }
+
+
+  @override
+  String toString() {
+    return '$nombre $apellido   Edad: $edad';
+  }
+}
+
